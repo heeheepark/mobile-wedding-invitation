2636c37cee690b9063c490eb3823dff0717861a3
heeheepark <pjh9641@gmail.com>
2026-06-24 15:56:04 +0900

    feat: 배경음악 변경 및 자동재생 추가, 모달 여백 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import Account from "../components/Account";
 import Calendar from "../components/Calendar";
@@ -14,29 +14,53 @@ import "../styles/common.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faVolume, faVolumeOff } from "@fortawesome/free-solid-svg-icons";
-import backgroundMusic from "../assets/music/Spring_Flowers.mp3";
+import backgroundMusic from "../assets/music/The_Beauty_of_Love.mp3";
 
 function Home() {
   const [isPlaying, setIsPlaying] = useState(false);
-
   const audioRef = useRef<HTMLAudioElement>(null);
 
-  const handlePlayPause = async () => {
+  const playMusic = async () => {
     if (!audioRef.current) return;
 
     try {
-      if (isPlaying) {
-        audioRef.current.pause();
-        setIsPlaying(false);
-      } else {
-        audioRef.current.currentTime = audioRef.current.currentTime || 0;
-        await audioRef.current.play();
-        setIsPlaying(true);
-      }
+      await audioRef.current.play();
+      setIsPlaying(true);
     } catch (error) {
       console.error(error);
     }
   };
+
+  const pauseMusic = () => {
+    if (!audioRef.current) return;
+
+    audioRef.current.pause();
+    setIsPlaying(false);
+  };
+
+  const handlePlayPause = async () => {
+    if (isPlaying) {
+      pauseMusic();
+    } else {
+      await playMusic();
+    }
+  };
+
+  useEffect(() => {
+    const startMusic = async () => {
+      if (audioRef.current && audioRef.current.paused) {
+        await playMusic();
+      }
+    };
+
+    document.addEventListener("touchstart", startMusic, { once: true });
+    document.addEventListener("click", startMusic, { once: true });
+
+    return () => {
+      document.removeEventListener("touchstart", startMusic);
+      document.removeEventListener("click", startMusic);
+    };
+  }, []);
 
   return (
     <div>
